0a300022d5f186dbe4eff0c84e61ebd4f06db773
gadael_sedubun <gadael.sedubun@visionet.co.id>
2018-08-20 15:18:00 +0700

    commit

--- app/src/main/java/com/somedeveloperblog/onlinepushnotification/MainActivity.kt
+++ app/src/main/java/com/somedeveloperblog/onlinepushnotification/MainActivity.kt
@@ -5,8 +5,15 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    // google Oauth client id
+    val oauthClientId =
+            "136513988383-ngqnambc1qk0u37tqvebpb500142bc9c.apps.googleusercontent.com"
+
+    val apiKey = "AIzaSyBeP_orjaBXpMUiA8_hMFGi4Hm5WP5hI9Y"
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
